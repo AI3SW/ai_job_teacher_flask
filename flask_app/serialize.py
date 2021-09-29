c1d5e4a1bc8be38b228325efe6a5a4f6c90685ea
Kian Boon Koh <kianboonkoh@gmail.com>
2021-09-29 22:44:19 +0800

    add img field to get job endpoint

--- flask_app/serialize.py
+++ flask_app/serialize.py
@@ -1,5 +1,10 @@
-from flask_marshmallow import Marshmallow
+from pathlib import Path
 
+from flask import current_app
+from flask_marshmallow import Marshmallow
+from PIL import Image
+
+from flask_app.commons.util import image_to_base64
 from flask_app.database.database import Description, Job
 
 ma = Marshmallow()
@@ -16,6 +21,13 @@ class JobSchema(ma.SQLAlchemyAutoSchema):
         exclude = ('img_path',)
 
     descriptions = ma.Pluck(DescriptionSchema, "description", many=True)
+    img = ma.Method("serialize_img")
+
+    def serialize_img(self, obj: Job):
+        img_path = (Path(current_app.config["ASSETS_PATH"])
+                    / obj.img_path).resolve()
+        img = Image.open(img_path)
+        return image_to_base64(img)
 
 
 job_schema = JobSchema()
